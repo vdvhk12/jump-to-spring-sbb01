7bdd1b543726dc2cc835220611b2e9e8459cc9dc
vdvhk12 <vdvhk12@gmail.com>
2024-12-12 18:07:17 +0900

    test: Question 리스트 조회 기능 테스트

--- src/test/java/org/example/jtsb01/Jtsb01ApplicationTests.java
+++ src/test/java/org/example/jtsb01/Jtsb01ApplicationTests.java
@@ -3,6 +3,7 @@ package org.example.jtsb01;
 import static org.assertj.core.api.Assertions.*;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import org.example.jtsb01.question.Question;
 import org.example.jtsb01.question.QuestionRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -19,23 +20,48 @@ class Jtsb01ApplicationTests {
     @Test
     @DisplayName("Question Create Test")
     void test01() {
-        Question q1 = questionRepository.save(Question.builder()
+        //given
+        //when
+        Question question1 = questionRepository.save(Question.builder()
             .subject("sbb가 무엇인가요?")
             .content("sbb에 대해서 알고 싶습니다.")
             .createDate(LocalDateTime.now())
             .build());
 
-        Question q2 = questionRepository.save(Question.builder()
+        Question question2 = questionRepository.save(Question.builder()
             .subject("스프링부트 모델 질문입니다.")
             .content("id는 자동으로 생성되나요?")
             .createDate(LocalDateTime.now())
             .build());
 
         //then
-        assertThat(q1.getId()).isEqualTo(1);
-        assertThat(q2.getId()).isEqualTo(2);
-        assertThat(q1.getSubject()).isEqualTo("sbb가 무엇인가요?");
-        assertThat(q2.getContent()).isEqualTo("id는 자동으로 생성되나요?");
+        assertThat(question1.getId()).isEqualTo(1);
+        assertThat(question2.getId()).isEqualTo(2);
+        assertThat(question1.getSubject()).isEqualTo("sbb가 무엇인가요?");
+        assertThat(question2.getContent()).isEqualTo("id는 자동으로 생성되나요?");
+    }
+
+    @Test
+    @DisplayName("Question findAll Test")
+    void test02() {
+        //given
+        Question question1 = questionRepository.save(Question.builder()
+            .subject("sbb가 무엇인가요?")
+            .content("sbb에 대해서 알고 싶습니다.")
+            .createDate(LocalDateTime.now())
+            .build());
+
+        Question question2 = questionRepository.save(Question.builder()
+            .subject("스프링부트 모델 질문입니다.")
+            .content("id는 자동으로 생성되나요?")
+            .createDate(LocalDateTime.now())
+            .build());
+
+        //when
+        List<Question> questions = questionRepository.findAll();
+
+        //then
+        assertThat(questions).hasSize(2);
     }
 
 }
