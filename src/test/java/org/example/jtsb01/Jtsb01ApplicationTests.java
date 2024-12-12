2efaf2d31e112e218c6bf0b23ee768a273be915b
vdvhk12 <vdvhk12@gmail.com>
2024-12-12 23:39:39 +0900

    test: Answer로 Question 조회 테스트 추가

--- src/test/java/org/example/jtsb01/Jtsb01ApplicationTests.java
+++ src/test/java/org/example/jtsb01/Jtsb01ApplicationTests.java
@@ -314,4 +314,27 @@ class Jtsb01ApplicationTests {
         assertThat(result).isEmpty();
     }
 
+    @Test
+    @DisplayName("Question from Answer Test")
+    @Transactional
+    void test13() {
+        //given
+        // when
+        Question question = questionRepository.save(Question.builder()
+            .subject("스프링부트 모델 질문입니다.")
+            .content("id는 자동으로 생성되나요?")
+            .createDate(LocalDateTime.now())
+            .answerList(new ArrayList<>())
+            .build());
+
+        Answer answer = answerRepository.save(Answer.builder()
+            .content("네 자동으로 생성됩니다.")
+            .createDate(LocalDateTime.now())
+            .question(question)
+            .build());
+
+        //then
+        assertThat(answer.getQuestion().getId()).isEqualTo(question.getId());
+    }
+
 }
