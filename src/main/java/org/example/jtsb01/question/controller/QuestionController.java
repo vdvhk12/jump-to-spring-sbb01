ba1c720b372036180af8b793c3b695c222c670e0
vdvhk12 <vdvhk12@gmail.com>
2024-12-13 15:54:35 +0900

    feat: 질문 등록 기능 구현

--- src/main/java/org/example/jtsb01/question/controller/QuestionController.java
+++ src/main/java/org/example/jtsb01/question/controller/QuestionController.java
@@ -1,13 +1,17 @@
 package org.example.jtsb01.question.controller;
 
+import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.jtsb01.question.model.QuestionDto;
+import org.example.jtsb01.question.model.QuestionForm;
 import org.example.jtsb01.question.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -29,5 +33,19 @@ public class QuestionController {
         QuestionDto question = questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
+    }
+
+    @GetMapping("/create")
+    public String create(QuestionForm questionForm) {
+        return "question_form";
+    }
+
+    @PostMapping("/create")
+    public String create(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+        if(bindingResult.hasErrors()) {
+            return "question_form";
+        }
+        questionService.createQuestion(questionForm);
+        return "redirect:/question/list";
     }
 }
--- src/main/java/org/example/jtsb01/question/service/QuestionService.java
+++ src/main/java/org/example/jtsb01/question/service/QuestionService.java
@@ -1,9 +1,12 @@
 package org.example.jtsb01.question.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.jtsb01.global.exception.DataNotFoundException;
+import org.example.jtsb01.question.entity.Question;
 import org.example.jtsb01.question.model.QuestionDto;
+import org.example.jtsb01.question.model.QuestionForm;
 import org.example.jtsb01.question.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
 
@@ -20,5 +23,13 @@ public class QuestionService {
     public QuestionDto getQuestion(Long id) {
         return QuestionDto.fromEntity(questionRepository.findById(id)
             .orElseThrow(() -> new DataNotFoundException("Question not found")));
+    }
+
+    public void createQuestion(QuestionForm questionForm) {
+        questionRepository.save(Question.builder()
+            .subject(questionForm.getSubject())
+            .content(questionForm.getContent())
+            .createDate(LocalDateTime.now())
+            .build());
     }
 }
