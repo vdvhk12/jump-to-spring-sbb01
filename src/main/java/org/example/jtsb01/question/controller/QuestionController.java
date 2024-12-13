58f4d1e5dbf155059db0e6310149fb49ac13524f
vdvhk12 <vdvhk12@gmail.com>
2024-12-13 12:12:49 +0900

    feat: 질문 상세 페이지 기능 구현
    - 커스텀 예외를 만들어서 예외처리

--- src/main/java/org/example/jtsb01/question/controller/QuestionController.java
+++ src/main/java/org/example/jtsb01/question/controller/QuestionController.java
@@ -7,17 +7,27 @@ import org.example.jtsb01.question.service.QuestionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<QuestionDto> questionList = questionService.getList();
         model.addAttribute("questionList", questionList);
         return "question_list";
+    }
+
+    @GetMapping("/detail/{id}")
+    public String detail(@PathVariable("id") Long id, Model model) {
+        QuestionDto question = questionService.getQuestion(id);
+        model.addAttribute("question", question);
+        return "question_detail";
     }
 }
--- src/main/java/org/example/jtsb01/question/service/QuestionService.java
+++ src/main/java/org/example/jtsb01/question/service/QuestionService.java
@@ -2,6 +2,7 @@ package org.example.jtsb01.question.service;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.example.jtsb01.global.exception.DataNotFoundException;
 import org.example.jtsb01.question.model.QuestionDto;
 import org.example.jtsb01.question.repository.QuestionRepository;
 import org.springframework.stereotype.Service;
@@ -14,5 +15,10 @@ public class QuestionService {
 
     public List<QuestionDto> getList() {
         return questionRepository.findAll().stream().map(QuestionDto::fromEntity).toList();
+    }
+
+    public QuestionDto getQuestion(Long id) {
+        return QuestionDto.fromEntity(questionRepository.findById(id)
+            .orElseThrow(() -> new DataNotFoundException("Question not found")));
     }
 }
