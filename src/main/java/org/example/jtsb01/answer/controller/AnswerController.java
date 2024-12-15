3a8511de7904b59ff39eca771c58c3c483f7c92e
vdvhk12 <vdvhk12@gmail.com>
2024-12-15 22:01:54 +0900

    refactor: AnswerController create 메서드 중복 코드 메서드 사용

--- src/main/java/org/example/jtsb01/answer/controller/AnswerController.java
+++ src/main/java/org/example/jtsb01/answer/controller/AnswerController.java
@@ -3,7 +3,6 @@ package org.example.jtsb01.answer.controller;
 import jakarta.validation.Valid;
 import java.security.Principal;
 import lombok.RequiredArgsConstructor;
-import org.example.jtsb01.answer.entity.Answer;
 import org.example.jtsb01.answer.model.AnswerDto;
 import org.example.jtsb01.answer.model.AnswerForm;
 import org.example.jtsb01.answer.service.AnswerService;
@@ -11,8 +10,6 @@ import org.example.jtsb01.question.model.QuestionDto;
 import org.example.jtsb01.question.service.QuestionService;
 import org.example.jtsb01.user.model.SiteUserDto;
 import org.example.jtsb01.user.service.SiteUserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
@@ -31,7 +28,6 @@ import org.springframework.web.server.ResponseStatusException;
 @RequestMapping("/answer")
 public class AnswerController {
 
-    private static final Logger logger = LoggerFactory.getLogger(AnswerController.class);
     private final AnswerService answerService;
     private final QuestionService questionService;
     private final SiteUserService siteUserService;
@@ -40,25 +36,14 @@ public class AnswerController {
     @PostMapping("/create/{id}")
     public String create(@PathVariable("id") Long id, @Valid AnswerForm answerForm,
         BindingResult bindingResult, Model model, Principal principal) {
-
         QuestionDto question = questionService.getQuestion(id);
         if (bindingResult.hasErrors()) {
             model.addAttribute("question", question);
             return "question_detail";
         }
-
-        logger.info("principal : {}", principal);
-        if(principal instanceof OAuth2AuthenticationToken oauth2Token) {
-            OAuth2User oauth2User = oauth2Token.getPrincipal();
-            String name = (String) oauth2User.getAttributes().get("name");
-
-            SiteUserDto siteUser = siteUserService.getSiteUser(name);
-            answerService.createAnswer(id, answerForm, siteUser);
-        } else {
-            SiteUserDto siteUser = siteUserService.getSiteUser(principal.getName());
-            answerService.createAnswer(id, answerForm, siteUser);
-        }
-
+        String username = getUsernameFromPrincipal(principal);
+        SiteUserDto siteUser = siteUserService.getSiteUser(username);
+        answerService.createAnswer(id, answerForm, siteUser);
         return String.format("redirect:/question/detail/%s", id);
     }
 
