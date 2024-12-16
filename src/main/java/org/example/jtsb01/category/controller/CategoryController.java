6272ad1e373da7666ce7af8bf7cfbaad77996ea4
vdvhk12 <vdvhk12@gmail.com>
2024-12-16 12:55:57 +0900

    refactor: 불필요한 코드 삭제

--- src/main/java/org/example/jtsb01/category/controller/CategoryController.java
+++ src/main/java/org/example/jtsb01/category/controller/CategoryController.java
@@ -5,18 +5,12 @@ import java.security.Principal;
 import lombok.RequiredArgsConstructor;
 import org.example.jtsb01.category.model.CategoryForm;
 import org.example.jtsb01.category.service.CategoryService;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.server.ResponseStatusException;
 
 @Controller
 @RequiredArgsConstructor
